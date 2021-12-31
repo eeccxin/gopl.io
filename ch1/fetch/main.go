faa17c4e527aac0079be326571ed53889c194028
ccx <1490581824@qq.com>
2021-12-31 19:49:43 +0800

    练习1.7-1.9

--- ch1/fetch/main.go
+++ ch1/fetch/main.go
@@ -12,22 +12,31 @@ import (
 	"io/ioutil"
 	"net/http"
 	"os"
+	"strings"
 )
 
 func main() {
 	for _, url := range os.Args[1:] {
+		//练习1.8
+		hasHttp := strings.HasPrefix(url, "http://")
+		if !hasHttp {
+			url = "http://" + url
+			fmt.Printf("url: %v\n", url)
+		}
 		resp, err := http.Get(url)
 		if err != nil {
 			fmt.Fprintf(os.Stderr, "fetch: %v\n", err)
 			os.Exit(1)
 		}
 		b, err := ioutil.ReadAll(resp.Body)
+		// _, err = io.Copy(os.Stdout, resp.Body) ##直接copy，不用使用b作为缓存区
 		resp.Body.Close()
 		if err != nil {
 			fmt.Fprintf(os.Stderr, "fetch: reading %s: %v\n", url, err)
 			os.Exit(1)
 		}
-		fmt.Printf("%s", b)
+		fmt.Printf("%s\n", b)                        //直接将字节数据按照字符串的格式打印
+		fmt.Printf("resp.Status: %v\n", resp.Status) //打印响应状态码
 	}
 }
 
